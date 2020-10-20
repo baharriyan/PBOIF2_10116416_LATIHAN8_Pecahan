f67d735371493e7c4b84f1eda7b0d0042768c938
baharriyan <indonesiariyan@gmail.com>
2020-10-20 18:57:52 +0700

    first commit

---
+++ PBOIF2_10116416_LATIHAN8_Pecahan/src/pboif2_10116416_latihan8_pecahan/PBOIF2_10116416_LATIHAN8_Pecahan.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2_10116416_latihan8_pecahan;
+/**
+ *
+ * @author Riyan Bahar
+ * Nama  : Riyan Bahar
+ * Kelas : IF-2
+ * Nim   : 10116416
+ * 
+ */
+public class PBOIF2_10116416_LATIHAN8_Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float  : " + f);
+        System.out.println("Double : " + d);
+    }
+    
+}
